6c56a77f8d4bbfc33880864c43d0df1658165709
Kevin <Ohmanni64@gmail.com>
2014-05-11 19:20:47 -0400

    Initial

---
+++ android/src/com/aneebo/storyidea/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aneebo.storyidea.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aneebo.storyidea.StoryIdea;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StoryIdea(), config);
+	}
+}
---
+++ desktop/src/com/aneebo/storyidea/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aneebo.storyidea.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aneebo.storyidea.StoryIdea;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StoryIdea(), config);
+	}
+}
