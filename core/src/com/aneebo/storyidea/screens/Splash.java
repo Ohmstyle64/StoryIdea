2a734331065cbd63c5abf4e73bca3717ce8c5e88
Ohmstyle64 <Ohmanni64@yahoo.com>
2014-05-13 18:23:38 -0400

    Added splash image to splash screen.

--- core/src/com/aneebo/storyidea/screens/Splash.java
+++ core/src/com/aneebo/storyidea/screens/Splash.java
@@ -1,49 +1,62 @@
 package com.aneebo.storyidea.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Splash implements Screen {
 
+	private Sprite splash;
+	private SpriteBatch batch;
+	
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		splash.draw(batch);
+		batch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-
+		batch = new SpriteBatch();
+		splash = new Sprite(new Texture("img/sampleSplash.jpg"));
+		splash.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		//TODO: Add Tween
+		
+		
+		//TODO: Send to Loading Screen
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-
+		dispose();
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		batch.dispose();
 	}
 
 }
