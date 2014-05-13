392c31f8ed2267f242d00863e9a88fed9e15511d
Ohmstyle64 <Ohmanni64@yahoo.com>
2014-05-13 18:00:54 -0400

    App first goes to Splash Screen now.

--- core/src/com/aneebo/storyidea/StoryIdea.java
+++ core/src/com/aneebo/storyidea/StoryIdea.java
@@ -1,27 +1,16 @@
 package com.aneebo.storyidea;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.aneebo.storyidea.screens.Splash;
+import com.badlogic.gdx.Game;
 
-public class StoryIdea extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class StoryIdea extends Game {
+	
+	public static final String TITLE = "Story Idea";
+	public static final String VERSION = "0.0.0";
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//Start the app by loading the Splash Screen.
+		setScreen(new Splash());
 	}
 }
