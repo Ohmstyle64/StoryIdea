fc90dd0fd95dfdbd0469fbb93116984e9ca70bc1
Kevin <Ohmanni64@gmail.com>
2014-06-14 13:27:42 -0400

    Added required methods from Interface

--- desktop/src/com/aneebo/storyidea/desktop/social/SocialDesktop.java
+++ desktop/src/com/aneebo/storyidea/desktop/social/SocialDesktop.java
@@ -11,10 +11,9 @@ public class SocialDesktop implements SocialCodeI {
 	private boolean loggedIn;
 	private Array<SocialUser> friends;
 	private CircleList circleList;
+	private boolean initiated = false;
 	
-	public SocialDesktop() {
-
-	}
+	public SocialDesktop() {}
 	
 	@Override
 	public void login() {
@@ -23,9 +22,8 @@ public class SocialDesktop implements SocialCodeI {
 	}
 
 	@Override
-	public boolean sendMessage(String str) {
+	public void sendMessage(UserCircle uc, String str) {
 		System.out.println("Desktop: "+str);
-		return false;
 	}
 
 	@Override
@@ -45,14 +43,17 @@ public class SocialDesktop implements SocialCodeI {
 	}
 	@Override
 	public void initiate() {
-		System.out.println("Initiated");
-		loggedIn = true;
-		friends = new Array<SocialUser>(false, 2, SocialUser.class);
-		SocialUser f1 = new SocialUser(0, 1, "Kevin");
-		SocialUser f2 = new SocialUser(0, 2, "Eric");
-		friends.add(f1);
-		friends.add(f2);
-		circleList = new CircleList();
+		if(!initiated) {
+			System.out.println("Initiated");
+			loggedIn = true;
+			friends = new Array<SocialUser>(false, 2, SocialUser.class);
+			SocialUser f1 = new SocialUser(0, 1, "Kevin");
+			SocialUser f2 = new SocialUser(0, 2, "Eric");
+			friends.add(f1);
+			friends.add(f2);
+			circleList = new CircleList();
+			initiated = true;
+		}
 	}
 
 	@Override
@@ -69,6 +70,33 @@ public class SocialDesktop implements SocialCodeI {
 	@Override
 	public void saveCircleToCloud(UserCircle circles) {
 		circleList.addCircle(circles);
+	}
+
+	@Override
+	public boolean isInitialized() {
+		return initiated;
+	}
+
+	@Override
+	public String receiveMessage() {
+		return null;
+	}
+
+	@Override
+	public SocialUser getMeUser() {
+		return new SocialUser(0,1,"Rudman");
+	}
+
+	@Override
+	public UserCircle getTempCloudCircle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void requestToStartCircle(UserCircle uc) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
